e35f7648ff1d1c879baf8bca7333ef7703333492
ofrezucker <112804727+ofrezucker@users.noreply.github.com>
2023-06-02 09:28:35 +0300

    alfha

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let something = 0
-let to_shoke_or_not_to_shoke = 0
 pins.digitalWritePin(DigitalPin.P0, 1)
+let to_shoke_or_not_to_shoke = 0
+let _ = 0
 basic.forever(function () {
     if (ModuleWorld_Digital.Button(ModuleWorld_Digital.mwDigitalNum.P12P13, ModuleWorld_Digital.enButton.Press)) {
         to_shoke_or_not_to_shoke = 1
@@ -8,34 +9,36 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    to_shoke_or_not_to_shoke = 0
+    _ = 0
     if (ModuleWorld_Digital.Button(ModuleWorld_Digital.mwDigitalNum.P14P15, ModuleWorld_Digital.enButton.Press)) {
-        for (let index = 0; index < 100; index++) {
+        for (let index = 0; index < 10; index++) {
             if (to_shoke_or_not_to_shoke == 1) {
                 music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
                 basic.showIcon(IconNames.Sad)
-                music.playMelody("C D E F C D E F ", 120)
                 basic.showString("have a good morning")
                 basic.pause(100)
                 TM1650.off()
                 to_shoke_or_not_to_shoke = 0
                 something = 10
+                _ = 1
+                break;
             } else {
                 music.playTone(262, music.beat(BeatFraction.Whole))
                 TM1650.on()
                 TM1650.showNumber(something)
                 something += -1
+                basic.pause(1000)
             }
         }
-    }
-    if (to_shoke_or_not_to_shoke == 0) {
-        music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
-        basic.showIcon(IconNames.Happy)
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        basic.pause(1000)
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        music.playMelody("C5 B A G C5 B A G ", 120)
-        basic.showString("have a WET morning")
-    } else {
-    	
+        if (to_shoke_or_not_to_shoke == 0 && _ == 0) {
+            music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
+            basic.showIcon(IconNames.Happy)
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            basic.pause(1000)
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            basic.showString("have a WET morning")
+        }
     }
 })
