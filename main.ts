e1e6f2007d419a16eaf3a8d52a6f65fc3966365f
ofrezucker <112804727+ofrezucker@users.noreply.github.com>
2023-06-06 13:49:30 +0300

    alpha 2.1

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ pins.digitalWritePin(DigitalPin.P0, 1)
 let to_shoke_or_not_to_shoke = 0
 let _ = 0
 let something = 10
+TM1650.on()
+music.setVolume(255)
 basic.forever(function () {
     if (ModuleWorld_Digital.Button(ModuleWorld_Digital.mwDigitalNum.P12P13, ModuleWorld_Digital.enButton.Press)) {
         to_shoke_or_not_to_shoke = 1
@@ -16,28 +18,26 @@ basic.forever(function () {
         for (let index = 0; index < 10; index++) {
             if (to_shoke_or_not_to_shoke == 1) {
                 music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
-                basic.showIcon(IconNames.Sad)
-                basic.showString("have a good morning")
                 basic.pause(100)
-                TM1650.off()
                 to_shoke_or_not_to_shoke = 0
                 something = 10
                 _ = 1
                 break;
+TM1650.showSring("have a good morning")
+                TM1650.clear()
             } else {
                 music.playTone(494, music.beat(BeatFraction.Whole))
-                basic.showNumber(something)
+                TM1650.showNumber(something)
                 something += -1
-                basic.pause(100)
+                TM1650.showNumber(something)
+                basic.pause(500)
             }
         }
         if (to_shoke_or_not_to_shoke == 0 && _ == 0) {
             music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
-            basic.showIcon(IconNames.Happy)
             pins.digitalWritePin(DigitalPin.P0, 0)
             basic.pause(1000)
             pins.digitalWritePin(DigitalPin.P0, 1)
-            basic.showString("have a WET morning")
             something = 10
         }
     }
